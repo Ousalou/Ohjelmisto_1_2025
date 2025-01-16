d341db75c44bb87869ce7cb6b3252c3dd47c3fdf
Ousalou <osalonen@icloud.com>
2025-01-16 10:46:48 +0200

    m3 push

---
+++ moduuli_3/m3_01_kuhan_pituus.py
@@ -0,0 +1,18 @@
+# Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä.
+# Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen
+# ilmoittaen samalla käyttäjälle, montako senttiä alimmasta sallitusta pyyntimitasta puuttuu.
+# Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+kuha_str = input("Mikä on kuhan pituus senttimetreinä? ")
+kuha = float(kuha_str)
+
+while kuha < 0:
+    kuha_str = input("Mikä on kuhan pituus senttimetreinä? ")
+    kuha = float(kuha_str)
+#tarkista miten tähän saa kuhalle määritelmän, että ei ole numero? nolla toissijainen
+
+if kuha >= 37:
+    print("Voit pitää kalasi!")
+
+if kuha < 37:
+    print(f"Heitä kuha takaisin järveen. Se on {37-kuha}cm alamittainen!")
