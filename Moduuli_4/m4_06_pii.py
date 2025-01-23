5d104a6ca3ad7cedb5968d7c2b74891644f44f0e
Ousalou <osalonen@icloud.com>
2025-01-23 11:38:34 +0200

    m4 push

--- Moduuli_4/m4_06_pii.py
+++ Moduuli_4/m4_06_pii.py
@@ -5,14 +5,32 @@
 # Jos neliön sisälle arvotaan satunnaisesti suuri määrä pisteitä, osuu niistä myös ympyrän sisälle
 # likimain niin suuri osuus kuin ympyrän pinta-ala on neliön pinta-alasta eli πr^2/4,
 # joka (koska ympyrän säde on yksi) sievenee muotoon π/4.
-
+#
 #Tästä saadaan yksinkertainen menetelmä piin likiarvon laskemiseksi:
 # Arvotaan neliön B sisälle suuri määrä satunnaisissa kohdissa olevia pisteitä, esimerkiksi miljoona.
 # Olkoon N tämä pisteiden kokonaismäärä.
 # Jokaisesta neliön B sisään arvotusta pisteestä testataan vuorollaan, jääkö se myös yksikköympyrän A sisälle.
 # Olkoon n ympyrän sisälle jäävien pisteiden kokonaismäärä. Nyt pätee n/N≈π/4, josta saadaan π≈4n/N.
-
+#
 #Kirjoita ohjelma, joka kysyy arvottavien pisteiden määrän käyttäjältä ja toteuttaa piin likiarvon laskennan edellä kuvatulla menetelmällä.
 # Lopuksi ohjelma tulostaa piin likiarvon käyttäjälle.
 # (Huomaa, että jokaisesta arvotusta pisteestä (x,y) on helppoa testata onko se yksikköympyrän A sisällä:
 # riittää testata, toteuttaako piste epäyhtälön x^2+y^2<1.)
+
+import random
+pisteetN = 1000000
+pisteet_n = 0
+
+while pisteetN < 1000001:
+    pisteetN = pisteetN - 1
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    if x**2+y**2<1:
+        pisteet_n = pisteet_n + 1
+    if pisteetN == 0:
+        break
+
+print(f"Pii on {pisteet_n*4 / 1000000}.")
+
+
+
