00afbae0c24a1700fe9b4e98cabd4e285a7e9c59
Ousalou <osalonen@icloud.com>
2025-01-29 13:20:12 +0200

    m6 push

--- moduuli_6/m6_02_tahkojen_yhteismaara.py
+++ moduuli_6/m6_02_tahkojen_yhteismaara.py
@@ -1,1 +1,20 @@
-u
+#Muokkaa edellistä funktiota siten,
+# että funktio saa parametrinaan nopan tahkojen yhteismäärän.
+# Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
+# Edellisestä tehtävästä poiketen
+# nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
+# joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+import random
+
+def nopan_heitto (noppa):
+    tulos = random.randint(1,noppa)
+    return tulos
+
+noppa = int(input("Monta tahkoa nopassa on? "))
+
+while True:
+    tulos = nopan_heitto(noppa)
+    print(f"Heitetään noppaa: {tulos}")
+    if tulos == noppa:
+        break
