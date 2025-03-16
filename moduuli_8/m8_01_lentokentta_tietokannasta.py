6e895fe1d23c16c1e46ccb18a8a3d1ce05f3a36d
Ousalou <osalonen@icloud.com>
2025-03-16 23:01:08 +0200

    m8 uusi push

--- moduuli_8/m8_01_lentokentta_tietokannasta.py
+++ moduuli_8/m8_01_lentokentta_tietokannasta.py
@@ -1,6 +1,5 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin.
-# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen j
-# a sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
+# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
 # ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
 
 import mysql.connector
@@ -9,8 +8,16 @@ yhteys = mysql.connector.connect(
          host='localhost',
          port= 3306,
          database='flight_game',
-         user='dbuser',
-         password='sAL_a3ana',
+         user='keltanokat',
+         password='lentopeli',
          autocommit=True
          )
+
+icao = input("What is the icao code for your airport? ")
+sql = f"select name, municipality from airport where ident = '{icao}'"
+cursor = yhteys.cursor()
+cursor.execute(sql)
+airport_and_name = cursor.fetchall()
+print(airport_and_name)
+
 
